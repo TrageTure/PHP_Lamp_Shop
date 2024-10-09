832f05484ea892454dad99666f34a70d854c9a16
Arthuritito <r1005702@student.thomasmore.be>
2024-10-09 10:13:27 +0200

    feat: changed title home

--- views/index.php
+++ views/index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Home</title>
 </head>
 <body>
     <?php include_once("../components/nav.php"); ?> 
