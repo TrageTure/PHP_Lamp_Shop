68ffb796e27c9e50816de7416203c30a7da4a795
Arthuritito <r1005702@student.thomasmore.be>
2024-11-03 13:43:40 +0100

    git commit -m "feat: made changes to the product details page"

--- views/index.php
+++ views/index.php
@@ -1,19 +1,27 @@
 <?php
-    session_start();
-    if ($_SESSION['loggedin'] !== true) {
-        header('location: login.php');
-    }
-    include_once('../components/db.php');
-    // $stmt = $conn->prepare('SELECT * FROM products');
-    // $stmt->execute();
-    // $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+session_start();
+if ($_SESSION['loggedin'] !== true) {
+    header('location: login.php');
+}
+include_once('../classes/Db.php');
+include_once('../classes/Product.php');
+include_once('../classes/Pictures.php');
 
+$product = new Product();
+
+if (!empty($_GET['category'])) {
+    $products = $product->getProductsByCategory($_GET['category']);
+} else {
+    $products = $product->getAllProducts();
+}
+
+$categories = $product->getAllCategories(); 
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/index.css">
@@ -21,107 +29,33 @@
 </head>
 <body>
     <?php include_once("../components/nav.php"); ?> 
-    <!-- <?php foreach($products as $p):?>
-    <article>
-        <h2><?php echo $p['title']?>: <?php echo $p['price']?>€</h2>
-    </article>
-    <?php endforeach?> -->
+
     <section class="filter">
-        <a href="">All</a>
-        <a href="">Wand</a>
-        <a href="">Vloer</a>
-        <a href="">Tafel</a>
-        <a href="">Plafond</a>
+        <a href="index.php">All</a>
+        <?php foreach ($categories as $category): ?>
+            <a href="index.php?category=<?php echo $category['id']; ?>">
+                <?php echo htmlspecialchars($category['title']); ?>
+            </a>
+        <?php endforeach; ?>
     </section>
 
     <section class="products">
-        <article>
-            <div class="product_img groen"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
-
-        <article>
-            <div class="product_img geel"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
-
-        <article>
-            <div class="product_img blauw"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
-
-        <article>
-            <div class="product_img geel"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
-
-        <article>
-            <div class="product_img blauw"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
-
-        <article>
-            <div class="product_img blauw"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
-
-        <article>
-            <div class="product_img groen"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
-
-        <article>
-            <div class="product_img geel"></div>
-            <div class="info_container">
-                <h2 class="name">Lamp groen</h2>
-                <p class="price">44.99$</p>
-                <p class="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In distinctio ab necessitatibus quos ullam perspiciatis dignissimos quas impedit qui laudantium, officia culpa, quod quidem nisi delectus ipsa repellendus autem optio?
-                </p>
-            </div>
-        </article>
+        <?php foreach($products as $p): ?>
+            <?php
+            $thumb = new Images();
+            $thumbnail = $thumb->getThumbnailByProductId($p['id']);
+            ?>
+            <article onclick="window.location.href='product_details.php?id=<?php echo $p['id']; ?>';">
+                <img src='../images/product_images/<?php echo htmlspecialchars($thumbnail); ?>' class="product_img">
+                <div class="info_container">
+                    <h2 class="name"><?php echo htmlspecialchars($p['title']); ?></h2>
+                    <p class="price"><?php echo htmlspecialchars($p['price']); ?>$</p>
+                    <p class="description">
+                        <?php echo htmlspecialchars($p['description']); ?>
+                    </p>
+                </div>
+            </article>
+        <?php endforeach; ?>
     </section>
 </body>
 </html>
