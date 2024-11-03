ced7b4eaa024cf471819d420fba0c544cdb549eb
Arthuritito <r1005702@student.thomasmore.be>
2024-11-03 13:45:54 +0100

    git commit -m "Add new file product_details.js"

---
+++ js/product_details.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const colorRadios = document.querySelectorAll('input[name="color"]');
+    const sizeRadios = document.querySelectorAll('input[name="size"]');
+    const stockDisplay = document.getElementById('stock_display');
+
+    function updateStock() {
+        const selectedColor = document.querySelector('input[name="color"]:checked');
+        const selectedSize = document.querySelector('input[name="size"]:checked');
+
+        if (selectedColor && selectedSize) {
+            const colorId = selectedColor.value;
+            const sizeId = selectedSize.value;
+
+            fetch(`../process/get_stock.php?product_id=${productId}&color_id=${colorId}&size_id=${sizeId}`)
+                .then(response => response.json())
+                .then(data => {
+                    stockDisplay.textContent = `${data.stock_amount}`;
+                })
+                .catch(error => {
+                    console.error('Error fetching stock:', error);
+                    stockDisplay.textContent = 'Error fetching stock';
+                });
+        }
+    }
+
+    colorRadios.forEach(radio => radio.addEventListener('change', updateStock));
+    sizeRadios.forEach(radio => radio.addEventListener('change', updateStock));
+});
