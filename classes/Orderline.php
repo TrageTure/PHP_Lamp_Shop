bf987b1bcd284faa047376a42cef4db79afc25d6
Arthuritito <r1005702@student.thomasmore.be>
2024-11-09 18:00:42 +0100

    feat: add save method to Orderline class for database insertion of order line details

--- classes/Orderline.php
+++ classes/Orderline.php
@@ -56,5 +56,23 @@ class Orderline {
     public function setPricePerUnit($price_per_unit) {
         $this->price_per_unit = $price_per_unit;
     }
+
+    public function save() {
+        try {
+            $conn = Db::getConnection();
+            $statement = $conn->prepare("INSERT INTO orderlines (order_id, product_id, amount, price_per_unit) VALUES (:order_id, :product_id, :amount, :price_per_unit)");
+            $order_id = $this->getOrderId();
+            $product_id = $this->getProductId();
+            $amount = $this->getAmount();
+            $price_per_unit = $this->getPricePerUnit();
+            $statement->bindParam(":order_id", $order_id);
+            $statement->bindParam(":product_id", $product_id);
+            $statement->bindParam(":amount", $amount);
+            $statement->bindParam(":price_per_unit", $price_per_unit);
+            $statement->execute();
+        } catch (Throwable $t) {
+            return $t->getMessage();
+        }
+    }
 }
 ?>
