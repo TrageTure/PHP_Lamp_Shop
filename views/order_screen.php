8a2a1aa40003d9f0e7592b066061cc138e3d776c
Arthuritito <r1005702@student.thomasmore.be>
2024-11-12 14:58:25 +0100

    feat: create order screen with styling and layout for displaying order items

---
+++ views/order_screen.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/all.css">
+    <link rel="stylesheet" href="../css/order.css">
+    <title>Order</title>
+</head>
+<body>
+    <?php include_once('../components/nav.php'); ?>
+    <main class="main">
+        <section class="orderlines">
+            <div class="item">
+                <img src="../images/product_images/67277a2ad8a95__DSC1549.jpg" alt="Cow in the sun">
+                <h1>Koe in zon</h1>
+                <div class="delete" aria-label="Delete item"></div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
