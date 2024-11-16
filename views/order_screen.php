4dd729e2fd804b35abfbb2eec4aca1f1801c6af1
Arthuritito <r1005702@student.thomasmore.be>
2024-11-16 14:18:24 +0100

    feat: enhance order screen layout with improved styling and delivery options

--- views/order_screen.php
+++ views/order_screen.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+// session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,14 +11,93 @@ session_start();
     <title>Order</title>
 </head>
 <body>
-    <?php include_once('../components/nav.php'); ?>
+    <?php include_once('../components/nav.php');?>
     <main class="main">
+
         <section class="orderlines">
-            <div class="item">
-                <img src="../images/product_images/67277a2ad8a95__DSC1549.jpg" alt="Cow in the sun">
-                <h1>Koe in zon</h1>
-                <div class="delete" aria-label="Delete item"></div>
+            <h1 class="artikelen_titel">Artikelen in winkelwagen:</h1>
+            <div class="order_item">
+                <img class="order_shopping_cart_item_img" src='../images/product_images/67277a2ad8a95__DSC1549.jpg'>
+                <div class="order_shopping_cart_info_grid">
+                    <div class="order_shopping_cart_item_name_color_size">
+                        <h1 class="order_shopping_cart_item_name">Koe in zon</h1>
+                        <p>Blauw, small</p>
+                    </div>
+                    <div class="order_delete" data-index=""></div> 
+                    <h2 class="order_shopping_cart_item_price">€20,99</h2>
+                    <p class="order_shopping_cart_item_amount">Aantal: 2</p>
+                </div>
             </div>
+            <div class="order_divider2"></div>
+
+            <div class="order_item">
+                <img class="order_shopping_cart_item_img" src='../images/product_images/67277a2ad8a95__DSC1549.jpg'>
+                <div class="order_shopping_cart_info_grid">
+                    <div class="order_shopping_cart_item_name_color_size">
+                        <h1 class="order_shopping_cart_item_name">Koe in zon</h1>
+                        <p>Blauw, small</p>
+                    </div>
+                    <div class="order_delete" data-index=""></div> 
+                    <h2 class="order_shopping_cart_item_price">€20,99</h2>
+                    <p class="order_shopping_cart_item_amount">Aantal: 2</p>
+                </div>
+            </div>
+            <div class="order_divider2"></div>
+
+            <div class="order_item">
+                <img class="order_shopping_cart_item_img" src='../images/product_images/67277a2ad8a95__DSC1549.jpg'>
+                <div class="order_shopping_cart_info_grid">
+                    <div class="order_shopping_cart_item_name_color_size">
+                        <h1 class="order_shopping_cart_item_name">Koe in zon</h1>
+                        <p>Blauw, small</p>
+                    </div>
+                    <div class="order_delete" data-index=""></div> 
+                    <h2 class="order_shopping_cart_item_price">€20,99</h2>
+                    <p class="order_shopping_cart_item_amount">Aantal: 2</p>
+                </div>
+            </div>
+            <div class="order_divider2"></div>
+
+        </section>
+
+        <section class="left">
+            <h1 class="title_left">Overzicht:</h1>
+            <div class="delivery_options">
+                <h2>Levering:</h2>
+                <div class="delivery_radio"> 
+                    <div class="standard">
+                        <input id="standard" type="radio" name="delivery" value="standard" checked/>
+                        <label for="standard"></label>
+                        <div class="flex_delivery_grid">
+                            <h3>Standard (gratis)</h3>
+                            <p>Ten laatste geleverd binnen 7 werkdagen</p>
+                        </div>
+                    </div>
+                    <div class="express">
+                        <input id="express" type="radio" name="delivery" value="express" />
+                        <label for="express"></label>
+                        <div class="flex_delivery_grid">
+                            <h3>Express (€7.99)</h3>
+                            <p>Ten laatste geleverd binnen 3 werkdagen</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="total_price">
+                <h2>Subtotaal:</h2>
+                <h2>€62,97</h2>
+            </div>
+            <div class="total_price">
+                <h2>Verzendkosten:</h2>
+                <h2>€62,97</h2>
+            </div>
+            <div class="total_price">
+                <h2>Totaal bedrag:</h2>
+                <h2>€62,97</h2>
+            </div>
+
+            <h3 class="order_btn_bestellen">Bestellen!</h3>
         </section>
     </main>
 </body>
