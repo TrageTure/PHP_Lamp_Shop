351a11bedbf6c229e54d25d30c2cd383cc6efdc2
Arthuritito <r1005702@student.thomasmore.be>
2024-12-07 21:50:39 +0100

    feat: implement SSL support for Azure MySQL database connection

--- classes/Db.php
+++ classes/Db.php
@@ -4,26 +4,32 @@ class Db {
 
     public static function connect() { // this method is used to connect to the database
         if (self::$conn === null) { // if the connection is not yet established
-            try {
                 // Update the connection details for Azure MySQL
                 $host = 'php-lamp-shop-server.mysql.database.azure.com';
                 $dbname = 'php-lamp-shop-database';
                 $username = 'tgpwrugenq'; // Vervang door jouw gebruikersnaam
                 $password = 'nH$DrdFPn85MIbbo'; // Vervang door jouw wachtwoord
                 $port = '3306'; // Standaard MySQL-poort
+                $sslCert = '../DigiCertGlobalRootCA.crt.pem';
 
-                // Establish the connection with SSL enabled
-                $dsn = "mysql:host=$host;dbname=$dbname;port=$port;charset=utf8;sslmode=require";
-                self::$conn = new PDO($dsn, $username, $password);
-                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                try {
+                    self::$conn = new PDO(
+                        "mysql:host=$host;dbname=$dbName;charset=utf8mb4",
+                        $username,
+                        $password,
+                        [
+                            PDO::MYSQL_ATTR_SSL_CA => $sslCert,
+                            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                        ]
+                    );
+                    return self::$conn;
+                } catch (PDOException $e) {
+                    die('Database connection failed: ' . $e->getMessage());
+                }
+            } else {
                 return self::$conn;
-            } catch (PDOException $e) {
-                // Error handling
-                die("Database connection failed: " . $e->getMessage());
             }
-        } else {
-            return self::$conn;
-        }
     }
 }
 ?>
