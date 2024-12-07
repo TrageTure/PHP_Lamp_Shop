4aac585e8b0a81feb74d723d58990168b056210b
Arthuritito <r1005702@student.thomasmore.be>
2024-12-07 21:54:45 +0100

    fix: correct variable name for database connection in Db.php

--- classes/Db.php
+++ classes/Db.php
@@ -4,32 +4,32 @@ class Db {
 
     public static function connect() { // this method is used to connect to the database
         if (self::$conn === null) { // if the connection is not yet established
-                // Update the connection details for Azure MySQL
-                $host = 'php-lamp-shop-server.mysql.database.azure.com';
-                $dbname = 'php-lamp-shop-database';
-                $username = 'tgpwrugenq'; // Vervang door jouw gebruikersnaam
-                $password = 'nH$DrdFPn85MIbbo'; // Vervang door jouw wachtwoord
-                $port = '3306'; // Standaard MySQL-poort
-                $sslCert = '../DigiCertGlobalRootCA.crt.pem';
+            // Update the connection details for Azure MySQL
+            $host = 'php-lamp-shop-server.mysql.database.azure.com';
+            $dbname = 'php-lamp-shop-database'; // Correcte variabele naam
+            $username = 'tgpwrugenq'; // Vervang door jouw gebruikersnaam
+            $password = 'nH$DrdFPn85MIbbo'; // Vervang door jouw wachtwoord
+            $port = '3306'; // Standaard MySQL-poort
+            $sslCert = '../DigiCertGlobalRootCA.crt.pem';
 
-                try {
-                    self::$conn = new PDO(
-                        "mysql:host=$host;dbname=$dbName;charset=utf8mb4",
-                        $username,
-                        $password,
-                        [
-                            PDO::MYSQL_ATTR_SSL_CA => $sslCert,
-                            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                        ]
-                    );
-                    return self::$conn;
-                } catch (PDOException $e) {
-                    die('Database connection failed: ' . $e->getMessage());
-                }
-            } else {
+            try {
+                self::$conn = new PDO(
+                    "mysql:host=$host;dbname=$dbname;charset=utf8mb4", // Gebruik $dbname
+                    $username,
+                    $password,
+                    [
+                        PDO::MYSQL_ATTR_SSL_CA => $sslCert,
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
                 return self::$conn;
+            } catch (PDOException $e) {
+                die('Database connection failed: ' . $e->getMessage());
             }
+        } else {
+            return self::$conn;
+        }
     }
 }
 ?>
