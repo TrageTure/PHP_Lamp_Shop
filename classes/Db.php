170a2450383629fede6cc966c8ea0e983b135004
Arthuritito <r1005702@student.thomasmore.be>
2024-12-07 21:23:39 +0100

    feat: update database connection method for Azure MySQL with SSL support

--- classes/Db.php
+++ classes/Db.php
@@ -1,13 +1,27 @@
 <?php 
-class Db{
+class Db {
     private static $conn;
 
-    public static function connect(){//this method is used to connect to the database
-        if (self::$conn === null) {//if the connection is not yet established
-            self::$conn = new PDO('mysql:host=localhost;dbname=LampShopXD', 'root', 'root');;//establish the connection
-            return self::$conn;
-        }
-        else{
+    public static function connect() { // this method is used to connect to the database
+        if (self::$conn === null) { // if the connection is not yet established
+            try {
+                // Update the connection details for Azure MySQL
+                $host = 'php-lamp-shop-server.mysql.database.azure.com';
+                $dbname = 'php-lamp-shop-database';
+                $username = 'tgprwugenq'; // Vervang door jouw gebruikersnaam
+                $password = 'nH$DRdFPn85MIbbo'; // Vervang door jouw wachtwoord
+                $port = '3306'; // Standaard MySQL-poort
+
+                // Establish the connection with SSL enabled
+                $dsn = "mysql:host=$host;dbname=$dbname;port=$port;charset=utf8;sslmode=require";
+                self::$conn = new PDO($dsn, $username, $password);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return self::$conn;
+            } catch (PDOException $e) {
+                // Error handling
+                die("Database connection failed: " . $e->getMessage());
+            }
+        } else {
             return self::$conn;
         }
     }
